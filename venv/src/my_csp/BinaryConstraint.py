101e7775400c8cccb40599015d1c8e0fe57721a0
AbedMir31 <abedmir31@gmail.com>
2020-03-12 22:37:01 -0500

    Edit constraint and CSP

--- venv/src/my_csp/BinaryConstraint.py
+++
@@ -1,6 +0,0 @@
-from typing import Dict, List, TypeVar
-from my_csp.CSP import Constraint
-
-VAR1, OP, VAR2 = TypeVar
-
-
--- venv/src/my_csp/CSP.py
+++ venv/src/my_csp/CSP.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar, Dict, List, Optional
+from typing import Generic, TypeVar, Dict, List
 
 V = TypeVar('V')
 D = TypeVar('D')
@@ -14,15 +14,31 @@ class BinaryConstraint:
         if self.left_var not in assignment or self.right_var not in assignment:
             return True
         else:
-            print("TESTING " + self.left_var + self.op + self.right_var)
+            print("Testing " + self.left_var + "=" + assignment[self.left_var] + self.op + self.right_var +
+                  '=' + assignment[self.right_var])
             if self.op == '<':
                 return assignment[self.left_var] < assignment[self.right_var]
             elif self.op == '>':
-                return assignment[self.left_var] < assignment[self.right_var]
+                return assignment[self.left_var] > assignment[self.right_var]
             elif self.op == '=':
                 return assignment[self.left_var] == assignment[self.right_var]
             elif self.op == '!':
                 return assignment[self.left_var] != assignment[self.right_var]
+
+
+def print_assignment(assignment):
+    for var in assignment:
+        print("%s=%s " % (var, assignment[var]), end='')
+    print('')
+
+
+def assign(var, value, assignment):
+    assignment[var] = value
+
+
+def unassign(var, assignment):
+    if var in assignment:
+        del assignment[var]
 
 
 class CSP(Generic[V, D]):
@@ -34,28 +50,46 @@ class CSP(Generic[V, D]):
     def add_constraint(self, constraint):
         self.constraints.append(constraint)
 
-    def check_valid(self, assignment: Dict[V, D]) -> bool:
+    def check_valid(self, var, value, assignment: Dict[V, D]) -> bool:
+        test = assignment.copy()
+        assign(var, value, test)
         for constraint in self.constraints:
-            if not constraint.isValid(assignment):
+            if not constraint.isValid(test):
                 return False
         return True
 
-    def backtracking_search(self, assignment: Dict[V, D] = None) -> Optional[Dict[V, D]]:
+    def choose_var(self, assignment):
+        unassigned = [v for v in self.variables if v not in assignment]
+        minlegalvar = unassigned[0]
+        minlegalval = len(self.domains[unassigned[0]])
+        for var in unassigned:
+            if len(self.domains[var]) < minlegalval:
+                minlegalvar = var
+                minlegalval = len(self.domains[var])
+        return minlegalvar
+
+    def backtracking_search(self, forward_check: bool):
+        if forward_check:
+            ...
+        return self.recursive_backtrack({})
+
+    def recursive_backtrack(self, assignment):
         if assignment is None:
             assignment = {}
             print("Initial Assignment: %s" % assignment)
         if len(assignment) == len(self.variables):
             print("return assignment")
             return assignment
-        unassigned: List[V] = [variable for variable in self.variables if variable not in assignment]
-        current_var = unassigned[0]
+        current_var = self.choose_var(assignment)
         for value in self.domains[current_var]:
-            if self.check_valid(assignment):
+            print("Going to check " + current_var + " and " + value)
+            if self.check_valid(current_var, value, assignment):
+                print("Check passed")
                 assignment[current_var] = value
-                result = self.backtracking_search(assignment)
+                result = self.recursive_backtrack(assignment)
                 if result is not None:
                     return result
-            else:
-                print(assignment)
-                assignment.pop(current_var)
+                unassign(current_var, assignment)
+            print("Check failed")
+        print_assignment(assignment)
         return None
